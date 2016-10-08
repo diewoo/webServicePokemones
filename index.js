b38513654dc7755aae30cb69a0a0f20bea7406a5
diewoo <dgo250594@gmail.com>
2016-10-07 23:16:53 -0500

    1%

--- index.js
+++ index.js
@@ -5,34 +5,21 @@ var app = express();
 const mongoose=require('mongoose'),
 	Schema = mongoose.Schema,
         ObjectId =  Schema.ObjectId;
-		
+
 const uri ="mongodb://diego:12345@ds053196.mlab.com:53196/ulima-moviles";
-	
+
 
 var schemaUsuario = new Schema({
     id : ObjectId,
     user : String,
-    pass : String 
+    pass : String
 });
 
 var Usuario = mongoose.model('usuarios',schemaUsuario);
-/*
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});*/
-
 app.set('port', (process.env.PORT || 5002));
 
-app.get('/insert', function(req,res) {	
-  
+app.get('/insert', function(req,res) {
+
 	if(!mongoose.connection.readyState){
         mongoose.connect(uri);
     }
@@ -43,15 +30,15 @@ app.get('/insert', function(req,res) {
         usuario.save((err)=>{
             if(err) throw err
             console.log("Guardado con exito!");
-            mongoose.disconnect(); 
-            
+            mongoose.disconnect();
+
         });
     });
 });
 
 
-app.get('/', function(req,res) {	
-  
+app.get('/', function(req,res) {
+
   if(!mongoose.connection.readyState){
         mongoose.connect(uri);
     }
@@ -60,23 +47,20 @@ app.get('/', function(req,res) {
     db.once('open', function callback () {
         Usuario.find({},(err,dato)=>{
 			console.log(dato);
-			res.send('Intento');
+			res.send(dato);
             mongoose.disconnect();
-			
+
         });
     });
 });
 
-  
+
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
 
 app.get('/cool', function(request, response) {
   response.send(cool());
